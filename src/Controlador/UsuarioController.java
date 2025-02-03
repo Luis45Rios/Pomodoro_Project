7b7dcb77f67690e85b5e9d8aab9c15db96e6e74d
Joseph Aguilar <josephadrianaguilar@gmail.com>
2025-02-02 21:32:39 -0500

    Elaboracion de los metodos para validar y crear usuario

--- src/Controlador/UsuarioController.java
+++ src/Controlador/UsuarioController.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package Controlador;
-
+import Modelos.Usuario;
+import Servicios.UsuarioFileManagment;
 /**
  *
- * @author luisr
+ * @author Joseph_Aguilar
  */
 public class UsuarioController {
-    
+    private final UsuarioFileManagment usuarioService = new UsuarioFileManagment();
+
+    public Boolean usuarioExiste(String username) {
+        return usuarioService.userExists(username);
+    }
+
+    public void crearUsuario(String username, String password) {
+        Usuario usuario = new Usuario(username, password);
+        usuarioService.createUser(usuario);
+    }
+
+    public Boolean validarUsuario(String username, String password) {
+        return usuarioService.validateUser(username, password);
+    }
 }
