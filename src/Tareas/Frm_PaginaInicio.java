b7f5ac477d47f5fe349e86a4a9ba87831507320c
Luis <luis.h.rios@unl.edu.ec>
2025-02-03 00:50:30 -0500

    Correcciones1

--- src/Tareas/Frm_PaginaInicio.java
+++ src/Tareas/Frm_PaginaInicio.java
@@ -24,20 +24,8 @@ public class Frm_PaginaInicio extends javax.swing.JFrame {
     private Usuario usuario;
     private static TareaController tareaController = new TareaController();
 
-    /**
-     * Creates new form frm_pagInicio
-     *
-     * @param usuario
-     */
     public Frm_PaginaInicio(Usuario usuario) {
         this.usuario = usuario;
-        initComponents();
-    }
-
-    /**
-     * Creates new form Frm_PaginaInicio
-     */
-    public Frm_PaginaInicio() {
         initComponents();
     }
 
@@ -50,113 +38,29 @@ public class Frm_PaginaInicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabelUsuario = new javax.swing.JLabel();
-        jButtonNuevaTarea = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTableResumen = new javax.swing.JTable();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jPanel1.setBackground(new java.awt.Color(209, 255, 184));
-        jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jPanel1.setPreferredSize(new java.awt.Dimension(750, 500));
-
-        jLabel1.setFont(new java.awt.Font("URW Bookman", 0, 60)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(55, 153, 22));
-        jLabel1.setText("Inicio");
-
-        jLabelUsuario.setFont(new java.awt.Font("URW Gothic", 0, 50)); // NOI18N
-        jLabelUsuario.setForeground(new java.awt.Color(1, 1, 1));
-        jLabelUsuario.setText("Bienvedido (Usuario) ");
-
-        jButtonNuevaTarea.setBackground(new java.awt.Color(55, 153, 22));
-        jButtonNuevaTarea.setFont(new java.awt.Font("URW Gothic", 0, 18)); // NOI18N
-        jButtonNuevaTarea.setForeground(new java.awt.Color(1, 1, 1));
-        jButtonNuevaTarea.setText("Nueva Tarea ");
-        jButtonNuevaTarea.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, null, new java.awt.Color(254, 254, 254), null, null));
-
-        jScrollPane2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jScrollPane2.setForeground(new java.awt.Color(1, 1, 1));
-
-        jTableResumen.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-
-            },
-            new String [] {
-                "Nombre", "Nro. De Pomodoros", "Tiempo de Enfoque"
-            }
-        ));
-        jScrollPane2.setViewportView(jTableResumen);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabelUsuario)
-                .addGap(114, 114, 114))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 613, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGap(468, 468, 468)
-                            .addComponent(jButtonNuevaTarea, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGap(288, 288, 288)
-                            .addComponent(jLabel1))))
-                .addContainerGap(69, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(43, 43, 43)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabelUsuario)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30)
-                .addComponent(jButtonNuevaTarea, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(29, Short.MAX_VALUE))
-        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 446, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButtonNuevaTareaActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jButtonNuevaTareaActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButtonNuevaTareaActionPerformed
         Frm_CrearTarea frm = new Frm_CrearTarea(this, rootPaneCheckingEnabled, usuario);
         setVisible(false);
         frm.setVisible(true);
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
-        // (optional) ">
-        /*
-                 * If Nimbus (introduced in Java SE 6) is not available, stay with the default
-                 * look and feel.
-                 * For details see
-                 * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
                     .getInstalledLookAndFeels()) {
@@ -170,18 +74,11 @@ public class Frm_PaginaInicio extends javax.swing.JFrame {
                     java.util.logging.Level.SEVERE, null,
                     ex);
         }
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
             new Frm_PaginaInicio(null).setVisible(true);
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonNuevaTarea;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabelUsuario;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTableResumen;
     // End of variables declaration//GEN-END:variables
 
     static class ButtonRenderer extends JButton implements TableCellRenderer {
