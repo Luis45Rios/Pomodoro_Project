fd3cbeae0745f885a3988035a6f4a0b51fb5617f
Domenica Rojas <domenica.rojas@unl.edu.ec>
2025-02-02 19:02:24 -0500

    Elaboración de los métodos crear, obtener, eliminar y actualizar Tarea en la clase TareaController

--- src/Controlador/TareaController.java
+++ src/Controlador/TareaController.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
+import java.util.List;
+
+import Modelos.Tarea;
+import Servicios.TareaFileManagement;
+
 /**
- *
- * @author luisr
+ * 
+ * @author domenica
  */
+
 public class TareaController {
-    
+    private final TareaFileManagement tareaService = new TareaFileManagement();
+
+    public Tarea crearTarea(String nombre, Integer tiempoEnfoque, Integer numeroPomodoros, String username) {
+        Integer id = tareaService.getTaskId();
+        Tarea tarea = new Tarea(id, nombre, tiempoEnfoque, numeroPomodoros, username);
+        tareaService.createTask(tarea);
+        return tarea;
+    }
+
+    public List<Tarea> obtenerTareas(String username) {
+        return tareaService.getTasks(username);
+    }
+
+    public void eliminarTarea(Integer id) {
+        tareaService.deleteTask(id);
+    }
+
+    public void actualizarTarea(Tarea tarea) {
+        tareaService.updateTask(tarea);
+    }
 }
